34b99e1e612dd44c71fa0cf8f408f1a02ed3eb43
dmnmkua <dmn.mk.ua@gmail.com>
2017-11-04 12:06:51 +0200

    final js_07

--- homework/js_07/js/main.js
+++ homework/js_07/js/main.js
@@ -3,54 +3,52 @@ window.addEventListener('DOMContentLoaded', () => {
   function SelectBlock() {
 
     // переменные
-    const list = document.querySelector('.block__list'),
-          item = document.querySelectorAll('.block__item'),
-          blockAttach = document.querySelectorAll('.block__attach'),
-          block1 = document.querySelector('#block_1'),
-          block2 = document.querySelector('#block_2'),
-          block3 = document.querySelector('#block_3');
+    this.list = document.querySelector('.block__list');
+    this.item = document.querySelectorAll('.block__item');
+    this.blockAttach = document.querySelectorAll('.block__attach');
 
     // функция создания блока в табах
     createTabBlock_ = (title, text, index) => {
-      let blockTitle = document.createElement('h1');
-      blockTitle.classList.add('block__attach-title');
-      let blockTitleText = document.createTextNode(title);
-      blockTitle.appendChild(blockTitleText);
-      let blockText = document.createElement('p');
-      blockText.classList.add('block__attach-desc');
-      let blockTextText = document.createTextNode(text)
-      blockText.appendChild(blockTextText);
-      blockAttach[index].appendChild(blockTitle);
-      blockAttach[index].appendChild(blockText);
+      this.blockTitle = document.createElement('h1');
+      this.blockTitle.classList.add('block__attach-title');
+      this.blockTitleText = document.createTextNode(title);
+      this.blockTitle.appendChild(this.blockTitleText);
+      this.blockText = document.createElement('p');
+      this.blockText.classList.add('block__attach-desc');
+      this.blockTextText = document.createTextNode(text);
+      this.blockText.appendChild(this.blockTextText);
+      this.blockAttach[index].appendChild(this.blockTitle);
+      this.blockAttach[index].appendChild(this.blockText);
     }
 
     // подгружаем первый блок
-    blockAttach[0].classList.add('block__attach--preloader');
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://ajax-menu.firebaseio.com/ajax2.json`);
-    xhr.responseType = 'json';
+    this.blockAttach[0].classList.add('block__attach--preloader');
+    this.xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('load', function() {
-      blockAttach[0].classList.remove('block__attach--preloader');
+    this.xhr.open('GET', `https://ajax-menu.firebaseio.com/ajax2.json`);
+    this.xhr.responseType = 'json';
 
-      if(xhr.status === 200 || xhr.status === 300) {
-        let data = xhr.response;
-        createTabBlock_(data[0].title, data[0].someText, 0);
+    this.xhr.addEventListener('load', () => {
+      this.blockAttach[0].classList.remove('block__attach--preloader');
+
+      if(this.xhr.status === 200 || this.xhr.status === 304) {
+        this.data = this.xhr.response;
+        createTabBlock_(this.data[0].title, this.data[0].someText, 0);
       }
       else {
-        alert(xhr.status + " : " + xhr.statusText);
+        alert(this.xhr.status + " : " + this.xhr.statusText);
       }
     })
-    xhr.send();
+    this.xhr.send();
 
     // функция сброса
     reset_ = () => {
-      for(let i = 0; i < item.length; i++) {
-        if(item[i].classList.contains('block__item--active')) {
-          item[i].classList.remove('block__item--active');
+      for(let i = 0; i < this.item.length; i++) {
+        if(this.item[i].classList.contains('block__item--active')) {
+          this.item[i].classList.remove('block__item--active');
         }
-        if(blockAttach[i].classList.contains('block__attach--active')) {
-          blockAttach[i].classList.remove('block__attach--active');
+        if(this.blockAttach[i].classList.contains('block__attach--active')) {
+          this.blockAttach[i].classList.remove('block__attach--active');
         }
       }
     }
@@ -59,36 +57,37 @@ window.addEventListener('DOMContentLoaded', () => {
     event_ = () => {
 
       // событие по клику на таб
-      list.addEventListener('click', (e) => {
+      this.list.addEventListener('click', (e) => {
 
         reset_();
 
         if(e.target.tagName === 'LI') {
-          for(let i = 0; i < item.length; i++) {
-            if(e.target === item[i]) {
-              if(blockAttach[i].childNodes.length === 0) {
-                blockAttach[i].classList.add('block__attach--preloader');
-                let xhr = new XMLHttpRequest();
-                xhr.open('GET', `https://ajax-menu.firebaseio.com/ajax2.json`);
-                xhr.responseType = 'json';
+          for(let i = 0; i < this.item.length; i++) {
+            if(e.target === this.item[i]) {
+              if(this.blockAttach[i].childNodes.length === 0) {
+                this.blockAttach[i].classList.add('block__attach--preloader');
+                this.xhr = new XMLHttpRequest();
 
-                xhr.addEventListener('load', function() {
-                  blockAttach[i].classList.remove('block__attach--preloader');
+                this.xhr.open('GET', `https://ajax-menu.firebaseio.com/ajax2.json`);
+                this.xhr.responseType = 'json';
 
-                  if(xhr.status === 200 || xhr.status === 300) {
-                    let data = xhr.response;
-                    createTabBlock_(data[i].title, data[i].someText, i)
+                this.xhr.addEventListener('load', () => {
+                  this.blockAttach[i].classList.remove('block__attach--preloader');
+
+                  if(this.xhr.status === 200 || this.xhr.status === 304) {
+                    this.data = this.xhr.response;
+                    createTabBlock_(this.data[i].title, this.data[i].someText, i)
                   }
                   else {
-                    alert(xhr.status + " : " + xhr.statusText);
+                    alert(this.xhr.status + " : " + this.xhr.statusText);
                   }
                 })
-                xhr.send();
+                this.xhr.send();
               }
 
-              item[i].classList.add('block__item--active');
+              this.item[i].classList.add('block__item--active');
               setTimeout( () => {
-                blockAttach[i].classList.add('block__attach--active');
+                this.blockAttach[i].classList.add('block__attach--active');
               }, 300);
             }
           }
